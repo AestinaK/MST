926520540a199f06bc788844c152b13dc2e32e90
Aestina Katwal <astinakatuwal9999@gmail.com>
2024-02-04 15:33:04 +0545

    implemented create method in controller

--- api fetch/Areas/Setup/Controller/IncomeCController.cs
+++ api fetch/Areas/Setup/Controller/IncomeCController.cs
@@ -1,3 +1,7 @@
+using api_fetch.Areas.Setup.ViewModel.IncomeC;
+using App.Setup.Dto;
+using App.Setup.Service.Interface;
+using AspNetCoreHero.ToastNotification.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api_fetch.Areas.Setup.Controller;
@@ -5,11 +9,42 @@ namespace api_fetch.Areas.Setup.Controller;
 [Area("setup")]
 public class IncomeCController : Microsoft.AspNetCore.Mvc.Controller
 {
-    
+    private readonly IIncomeCService _incomeCService;
+    private readonly INotyfService _notyfService;
+
+    public IncomeCController(IIncomeCService incomeCService,
+        INotyfService notyfService)
+    {
+        _incomeCService = incomeCService;
+        _notyfService = notyfService;
+    }
+
     [HttpGet]
     // GET
     public IActionResult Add()
     {
-        return View();
+        var vm = new IncomeCVm();
+        return View(vm);
+    }
+
+    [HttpPost]
+    public IActionResult Add(IncomeCVm vm)
+    {
+        try
+        {
+            var dto = new IncomeCDto()
+            {
+                Name = vm.Name,
+                Description = vm.Description
+            };
+            _incomeCService.CreateIncomeCategory(dto);
+            _notyfService.Success("Added");
+        }
+        catch (Exception e)
+        {
+            _notyfService.Error(e.Message);
+            return Redirect("/");
+        }
+        return RedirectToAction(nameof(Add));
     }
 }
---
+++ api fetch/Areas/Setup/ViewModel/IncomeC/IncomeCVm.cs
@@ -0,0 +1,7 @@
+namespace api_fetch.Areas.Setup.ViewModel.IncomeC;
+
+public class IncomeCVm
+{
+    public string Name { get; set; }
+    public string Description { get; set; } 
+}
